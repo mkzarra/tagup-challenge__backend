e87691d0ac69c192ef06194bddbebf74281a83b8
Mike Zarra <mkzarra@Mikes-Air.fios-router.home>
2020-11-17 17:07:56 -0500

    dog profile

--- client/src/components/pages/Dogs/DogDetails.js
+++ client/src/components/pages/Dogs/DogDetails.js
@@ -7,7 +7,8 @@ export default class DogDetails extends Component {
 	state = {
 		error: null,
 		loading: true,
-		dogDetails: null
+		dogDetails: null,
+		expandVaxList: false
 	}
 
 	componentDidMount() {
@@ -29,8 +30,18 @@ export default class DogDetails extends Component {
 		});
 	}
 
+	toggleVaxClassList = (vaxClassList) => {
+		vaxClassList.push(classes.ActiveVaccines)
+		this.setState((prevState) => {
+			return {
+				...prevState,
+				expandVaxList: !prevState.expandVaxList
+			}
+		});
+	}
+
 	render() {
-		const { dogDetails, error, loading } = this.state;
+		const { dogDetails, error, loading, expandVaxList } = this.state;
 		let profile = <Spinner />;
 
 		const householdCompatibility = [];
@@ -41,34 +52,36 @@ export default class DogDetails extends Component {
 			"dog": dogDetails.dogFriendly,
 			"cat": dogDetails.catFriendly,
 			"kid": dogDetails.kidFriendly
-		}
+			}
 
-		for (const pal in potentialHousemates) {
-			if (potentialHousemates[pal]) householdCompatibility.push(<p key={pal}>{pal} friendly</p>);
-		}
+			for (const pal in potentialHousemates) {
+				if (potentialHousemates[pal]) householdCompatibility.push(<p key={pal}>{pal} friendly</p>);
+			}
 
-		const isLeashTrained = dogDetails.leashTrained ? <p>Enjoys on-leash walks</p> : <p>Needs a patient handler willing to work on leash manners</p>;
-		const isHouseTrained = dogDetails.houseTrained ? <p>house trained</p> : <p>Chance of glee pees and oops poops</p>;
-		const vaccination = dogDetails.vaccines ? dogDetails.vaccines.map(vac => <p key={vac}>{vac}</p>) : null;
+			const vaxClasses = [classes.Vaccines];
 
+			const isLeashTrained = dogDetails.leashTrained ? <p>Enjoys on-leash walks</p> : <p>Needs a patient handler willing to work on leash manners</p>;
+			const isHouseTrained = dogDetails.houseTrained ? <p>house trained</p> : <p>Chance of glee pees and oops poops</p>;
+			const vaccination = dogDetails.vaccines ? dogDetails.vaccines.map(vac => <p key={vac}>{vac}</p>) : null;
+			
 			profile = (
 				<div className={classes.DogProfile}>
-					<div>
 						<div className={classes.ImageContainer}>
 							<img src={dogDetails.photoSrc} className={classes.Image} alt={dogDetails.name + " is a " + dogDetails.age + " year old " + dogDetails.gender + ", " + this.props.breed + "."}/>
+							<h1 className="right">{dogDetails.name}</h1>
 						</div>
-						<div className={classes.DetailsContainer}>
-							<h1>{dogDetails.name}</h1>
+					<div className={classes.Wrapper}>
+						<div className={vaxClasses.join(' ')} onClick={() => this.toggleVaxClassList(vaxClasses)}>
+							<h6>Vaccines</h6>
+							{vaccination}
+						</div>
+						<div className={[classes.DetailsContainer, 'right'].join(' ')}>
 							<h5>{dogDetails.breed}</h5>
-							<p>{dogDetails.name}</p>
 							<p>{dogDetails.gender}</p>
 							<p>{dogDetails.weight} lbs</p>
 							{householdCompatibility}
 							{isLeashTrained}
 							{isHouseTrained}
-							<div className={classes.Vaccines}>
-								{vaccination}
-							</div>
 						</div>
 					</div>
 				</div>
